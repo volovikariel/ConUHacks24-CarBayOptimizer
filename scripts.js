dbf8bac31b5889447d1fb6a626a944cde3e5a378
David Ruiz <josedruiz1@gmail.com>
2024-01-21 07:24:11 -0500

    first portion of the cars appearing

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
 
-tippy('[data-tippy-content]');
-tippy('button', {
-    duration: 0,
-    arrow: false,
-    delay: [1000, 200]
-  });
 
 var boxElement = document.createElement("div")
 
@@ -36,3 +28,30 @@ for (let index = 0; index < 10; index++) {
     boxElement.style.borderRadius = "10px";
     document.body.appendChild(boxElement)
 }
+
+function getAbsolutePosition(element) {
+    var rect = element.getBoundingClientRect();
+    return {
+      top: rect.top + window.scrollY,
+      left: rect.left + window.scrollX
+    };
+  }
+  var myTableCell = document.getElementById('testing_cell');
+    var absolutePosition = getAbsolutePosition(myTableCell);
+    console.log("hello")
+    console.log('Absolute Position:', absolutePosition);
+
+    function createOverlayImage(element) {
+        var overlayImage = new Image();
+        overlayImage.src = 'mechanic.png'; // Replace with the path to your overlay image
+        overlayImage.className = 'overlay-image';
+  
+        var position = getAbsolutePosition(element);
+        overlayImage.style.top = '0';
+        overlayImage.style.left = '0';
+  
+        element.appendChild(overlayImage);
+      }
+  
+createOverlayImage(myTableCell)
+

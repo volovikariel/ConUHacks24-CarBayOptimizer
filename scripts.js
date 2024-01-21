40fa09a2c6876f6748cedb74878d95938b2a7bd3
Yash Patel <yashpatel1815@hotmail.com>
2024-01-21 00:45:27 -0500

    added 10 red boxes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-var boxElement = document.createElement("div")
-
-boxElement.style.width = "130px";
-boxElement.style.height = "80px";
-boxElement.style.display = "inline-block";
-boxElement.style.backgroundColor = "#f00";
-boxElement.style.marginLeft = "190px";
-boxElement.style.marginTop = "280px"
-boxElement.style.position = "relative"
-boxElement.style.zIndex = "1";
-boxElement.style.borderRadius = "10px";
-
-document.body.appendChild(boxElement)
+for (let index = 0; index < 10; index++) {
+    let boxElement = document.createElement("div")
+    let marginTop = -71 + 4.4 * index;
+    console.log( marginTop + "%");
+    boxElement.style.width = "130px";
+    boxElement.style.height = "70px";
+    boxElement.style.display = "inline-block";
+    boxElement.style.backgroundColor = "#f00";
+    boxElement.style.marginLeft = "8%";
+    boxElement.style.marginTop = marginTop + "%";
+    boxElement.style.position = "absolute";
+    boxElement.style.zIndex = "1";
+    boxElement.style.borderRadius = "10px";
+    document.body.appendChild(boxElement)
+}

84a983ec3488f2201d31b35405be3862b7991009
volovikariel <16262535+volovikariel@users.noreply.github.com>
2024-01-27 19:48:07 -0500

    hopefully fix memoization fo get_max_revenue

--- backend/utils/schedule.py
+++ backend/utils/schedule.py
@@ -8,55 +8,60 @@ def schedule(jobs: list[Job]) -> (int, list[Job]):
     sorted_jobs = sorted(jobs, key=lambda j: j.finish)
 
     max_revenue, selected_job_idxs = get_max_revenue(
-        sorted_jobs, memo={}, considered_job_idx=len(sorted_jobs) - 1
+        sorted_jobs, considered_job_idx=len(sorted_jobs) - 1
     )
     return max_revenue, [sorted_jobs[job_idx] for job_idx in selected_job_idxs]
 
 
 def get_max_revenue(
     jobs: list[Job],
-    # ConsideredJobIdx -> (Revenue, Selected Jobs Idxs)
-    memo: dict[int, (int, list[int])],
     considered_job_idx: int,
 ) -> tuple[int, list[int]]:
-    # Assuming jobs is sorted by finish time
+    memo: dict[int, tuple[int, list[int]]] = {}
 
-    # Index out of bounds
-    if considered_job_idx < 0 or considered_job_idx >= len(jobs):
-        return 0, []
-    # we have a memo table to prevent duplicating calculations
-    if considered_job_idx in memo:
-        return memo[considered_job_idx]
-    # We can either take a job or not
-    take_job_revenue, selected_jobs_idx_taken = get_max_revenue(
-        jobs,
-        memo,
-        get_nearest_prev_finish_time(
-            [(job.start, job.finish) for job in jobs], considered_job_idx
-        ),
-    )
-    take_job_revenue += jobs[considered_job_idx].revenue
-    if jobs[considered_job_idx].treat_as_inf:
-        take_job_revenue = float("inf")
-        memo[considered_job_idx] = (
-            take_job_revenue,
-            selected_jobs_idx_taken + [considered_job_idx],
-        )
-        return memo[considered_job_idx]
-    skip_job_revenue, selected_jobs_idx_skipped = get_max_revenue(
-        jobs, memo, considered_job_idx - 1
-    )
+    def get_max_revenue_helper(
+        jobs: list[Job], considered_job_idx: int
+    ) -> tuple[int, list[int]]:
+        # Assuming jobs is sorted by finish time
+        if considered_job_idx in memo:
+            return memo[considered_job_idx]
 
-    # NOTE: Tiebreakers shouldn't matter?
-    if take_job_revenue >= skip_job_revenue:
-        memo[considered_job_idx] = (
-            take_job_revenue,
-            selected_jobs_idx_taken + [considered_job_idx],
+        # Index out of bounds
+        if considered_job_idx < 0 or considered_job_idx >= len(jobs):
+            return 0, []
+        # We can either take a job or not
+        take_job_revenue, selected_jobs_idx_taken = get_max_revenue_helper(
+            jobs,
+            get_nearest_prev_finish_time(
+                [(job.start, job.finish) for job in jobs], considered_job_idx
+            ),
         )
-    else:
-        memo[considered_job_idx] = (
-            skip_job_revenue,
-            selected_jobs_idx_skipped,
-        )
+        take_job_revenue += jobs[considered_job_idx].revenue
+        if jobs[considered_job_idx].treat_as_inf:
+            take_job_revenue = float("inf")
+            result = (
+                take_job_revenue,
+                selected_jobs_idx_taken + [considered_job_idx],
+            )
+        else:
+            skip_job_revenue, selected_jobs_idx_skipped = get_max_revenue_helper(
+                jobs, considered_job_idx - 1
+            )
 
-    return memo[considered_job_idx]
+            # NOTE: Tiebreakers shouldn't matter?
+            if take_job_revenue >= skip_job_revenue:
+                result = (
+                    take_job_revenue,
+                    selected_jobs_idx_taken + [considered_job_idx],
+                )
+            else:
+                result = (
+                    skip_job_revenue,
+                    selected_jobs_idx_skipped,
+                )
+
+        memo[considered_job_idx] = result
+        return result
+
+    result = get_max_revenue_helper(jobs, considered_job_idx)
+    return result
